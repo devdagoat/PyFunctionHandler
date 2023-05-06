c6bdcf30c403ed048df7be4e3e1c853cfacbc050
devdagoat <fidevran@hotmail.com>
2023-05-06 22:44:39 +0300

    Made code more readable

--- func_handler.py
+++ func_handler.py
@@ -1,15 +1,16 @@
 from typing import Callable,Any
 
 def handler(*func: Callable[...,Any], **func_kwds: dict[str,Any] | tuple[Any]) -> dict[str,Any]:
-    __returns = {}
-    for __func in func:
-        if __func.__name__ in func_kwds.keys():
-            __args = func_kwds[__func.__name__]
-            if type(__args) == tuple: #positional arguments
-                __returned = __func(*__args)
-            elif type(__args) == dict: #keyword arguments
-                __returned = __func(**__args)
+    _returns = {}
+    for _func in func:
+        _funcname = _func.__name__
+        if _funcname in func_kwds.keys():
+            _args = func_kwds[_funcname]
+            if type(_args) == tuple: #positional arguments
+                _returned = _func(*_args)
+            elif type(_args) == dict: #keyword arguments
+                _returned = _func(**_args)
         else:
-            __returned = __func()
-        __returns.update({__func.__name__ : __returned})
-    return __returns
+            _returned = _func()
+        _returns.update({_func.__name__ : _returned})
+    return _returns
